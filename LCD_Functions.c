cbf7a8ba092e02784a974406820879dae5a873cf
FadiiBassem <85284328+FadiiBassem@users.noreply.github.com>
2021-06-04 23:09:28 +0200

    Update LCD_Functions.c

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -1,3 +1,20 @@
+void LCD_init(void){
+  Serial.begin(9600);
+  SYSCTL_RCGCGPIO_R |= 0X03;
+  
+  GPIO_PORTA_DIR_R |= 0XE0;
+  GPIO_PORTA_DEN_R |= 0XE0;
+
+  GPIO_PORTB_DIR_R |= 0XFF;
+  GPIO_PORTB_DEN_R |= 0XFF;
+
+  LCD_Cmd(0X38);
+  LCD_Cmd(0x06);
+  LCD_Cmd(0X0F);
+  LCD_Cmd(0X01);
+
+}
+
 void LCD_Cmd(char command){
   GPIO_PORTA_DATA_R = 0X00;
   GPIO_PORTB_DATA_R = command;
