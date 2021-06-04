3025ca5b09a2cad849956ae1637b912fe46e5f4c
Omar Gamal <84602727+OmarGamal85@users.noreply.github.com>
2021-06-04 23:33:16 +0200

    Create LCD_Functions.c

---
+++ LCD_Functions.c
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void LCD_delay(int milliseconds){
+    for (int i = 0; i < milliseconds; i++)
+    {
+        for (int j = 0; j < 3180; j++)
+        {
+            
+        }
+        
+    }
+}
