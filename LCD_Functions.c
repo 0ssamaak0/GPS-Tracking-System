8543ca240b5669a1212a6493f9fa90c9f75d99b7
Omar Gamal <84602727+OmarGamal85@users.noreply.github.com>
2021-06-05 00:37:03 +0200

    Update LCD_Functions.c

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -1,3 +1,29 @@
+#include <stdint.h>
+
+/* define registers memory location */
+#define SYSCTL_RCGCGPIO_R       (*((volatile uint32_t *)0x400FE608))
+#define SYSCTL_PRGPIO_R         (*((volatile uint32_t *)0x400FEA08))
+
+#define GPIO_PORTA_DIR_R        (*((volatile uint32_t *)0x40004400))
+#define GPIO_PORTA_DEN_R        (*((volatile uint32_t *)0x4000451C))
+#define GPIO_PORTA_DATA_R       (*((volatile uint32_t *)0x400043FC))
+#define GPIO_PORTA_AMSEL_R      (*((volatile uint32_t *)0x40004528))
+#define GPIO_PORTA_AFSEL_R      (*((volatile uint32_t *)0x40004420))
+#define GPIO_PORTA_PCTL_R       (*((volatile uint32_t *)0x4000452C))
+#define GPIO_PORTA_CR_R         (*((volatile uint32_t *)0x40004524))
+
+#define GPIO_PORTB_DIR_R        (*((volatile uint32_t *)0x40005400))
+#define GPIO_PORTB_DEN_R        (*((volatile uint32_t *)0x4000551C))
+#define GPIO_PORTB_DATA_R       (*((volatile uint32_t *)0x400053FC))
+#define GPIO_PORTB_AMSEL_R      (*((volatile uint32_t *)0x40005528))
+#define GPIO_PORTB_AFSEL_R      (*((volatile uint32_t *)0x40005420))
+#define GPIO_PORTB_PCTL_R       (*((volatile uint32_t *)0x4000552C))
+#define GPIO_PORTB_CR_R         (*((volatile uint32_t *)0x40005524))
+
+#define NVIC_ST_CTRL_R          (*((volatile uint32_t *)0xE000E010))
+#define NVIC_ST_RELOAD_R        (*((volatile uint32_t *)0xE000E014))
+#define NVIC_ST_CURRENT_R       (*((volatile uint32_t *)0xE000E018))
+
 void LCD_init(){
 
     SYSCTL_RCGCGPIO_R = 0X03;               // 0000 0011
@@ -58,13 +84,16 @@ void LCD_set_Cursor(int line, int block){
 
 
 
-void LCD_delay(int milliseconds){
-    for (int i = 0; i < milliseconds; i++)
-    {
-        for (int j = 0; j < 3180; j++)
-        {
-            
-        }
-        
-    }
+void Systick_Timer(int counts, char mode[]){
+  NVIC_ST_CTRL_R = 0X00;
+  if(mode == "us"){
+    NVIC_ST_RELOAD_R = counts*(16-1);
+  }else if(mode == "ms"){
+    NVIC_ST_RELOAD_R = counts*(16000-1);
+  }
+  else if(mode == "sec"){
+    NVIC_ST_RELOAD_R = counts*(16000000-1);
+  }
+  NVIC_ST_CURRENT_R = 0;
+  NVIC_ST_CTRL_R = 5;
 }
