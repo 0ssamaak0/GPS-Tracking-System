6e11e3efdce03f2eaaffcf5b415d25e8b8e1eb92
FadiiBassem <85284328+FadiiBassem@users.noreply.github.com>
2021-06-04 19:06:29 +0200

    Update LCD_Functions.c

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -1,1 +1,16 @@
+void LCD_init(void)
+{
+  SYSCTL_RCGCGPIO_R |= 0X03;
+  while (!(SYSCTL_PRGPIO_R & 0X03)) {};
 
+  GPIO_PORTA_DIR_R |= 0XE0;
+  GPIO_PORTA_DEN_R |= 0XE0;
+
+  GPIO_PORTB_DIR_R |= 0XFF;
+  GPIO_PORTB_DEN_R |= 0XFF;
+
+  LCD_8Bit();
+  LCD_Move_Right();
+  LCD_Cursor_Off();
+  LCD_Clear()
+}
