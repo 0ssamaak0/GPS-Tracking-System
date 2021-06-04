c2ae810a9b35185d1a76d3ae73aa000e13e7354f
mokhtar-2000 <mokhtarhisham10@gmail.com>
2021-06-05 00:00:01 +0200

    add command lcd function

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -1,6 +1,16 @@
 
 
 
+void LCD_Cmd(char command){
+  GPIO_PORTA_DATA_R = 0X00;
+  GPIO_PORTB_DATA_R = command;
+  GPIO_PORTA_DATA_R |= 0X80;
+  delay(3);
+  GPIO_PORTA_DATA_R = 0X00;
+  
+}
+
+
 
 
 
