ee61943bc246f5d866285ccde3dcb353f4f0030c
Omar Gamal <84602727+OmarGamal85@users.noreply.github.com>
2021-06-04 23:59:11 +0200

    Update LCD_Functions.c

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -1,3 +1,29 @@
+#include <stdint.h>
+
+/* define registers memory location */
+#define SYSCTL_RCGCGPIO_R       (*((volatile uint32_t *)0x400FE608))
+#define SYSCTL_PRGPIO_R         (*((volatile uint32_t *)0x400FEA08))
+
+#define GPIO_PORTA_DIR_R        (*((volatile uint32_t *)0x40004400))
+#define GPIO_PORTA_DEN_R        (*((volatile uint32_t *)0x4000451C))
+#define GPIO_PORTA_DATA_R       (*((volatile uint32_t *)0x400043FC))
+#define GPIO_PORTA_AMSEL_R      (*((volatile uint32_t *)0x40004528))
+#define GPIO_PORTA_AFSEL_R      (*((volatile uint32_t *)0x40004420))
+#define GPIO_PORTA_PCTL_R       (*((volatile uint32_t *)0x4000452C))
+#define GPIO_PORTA_CR_R         (*((volatile uint32_t *)0x40004524))
+
+#define GPIO_PORTB_DIR_R        (*((volatile uint32_t *)0x40005400))
+#define GPIO_PORTB_DEN_R        (*((volatile uint32_t *)0x4000551C))
+#define GPIO_PORTB_DATA_R       (*((volatile uint32_t *)0x400053FC))
+#define GPIO_PORTB_AMSEL_R      (*((volatile uint32_t *)0x40005528))
+#define GPIO_PORTB_AFSEL_R      (*((volatile uint32_t *)0x40005420))
+#define GPIO_PORTB_PCTL_R       (*((volatile uint32_t *)0x4000552C))
+#define GPIO_PORTB_CR_R         (*((volatile uint32_t *)0x40005524))
+
+#define NVIC_ST_CTRL_R          (*((volatile uint32_t *)0xE000E010))
+#define NVIC_ST_RELOAD_R        (*((volatile uint32_t *)0xE000E014))
+#define NVIC_ST_CURRENT_R       (*((volatile uint32_t *)0xE000E018))
+
 
 // Write string on screen
 void LCD_Write(char Word[]) {
@@ -6,15 +32,7 @@ void LCD_Write(char Word[]) {
   LCD_Data(Word[i]);
 }
 
-// Takes a line (0, 1) and a block(0, 15)
-void LCD_set_Cursor(int line, int block){
-    if(line == 0){
-        LCD_command(0X80 + block);
-    }
-    else if (line == 1){
-        LCD_command(0xC0 + block);
-    }
-}
+
 
 
 
