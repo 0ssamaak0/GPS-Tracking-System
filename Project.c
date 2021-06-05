77f9ae13a82bd54e231aee8cd34b41665d9492e3
Hussein Ahmed <85221438+huss0123@users.noreply.github.com>
2021-06-05 21:06:00 +0200

    Update Project.c

--- Project.c
+++ Project.c
@@ -5,3 +5,21 @@ char Total_Dis[] = "127";
 char Total_Time[] = "389";
 char Current_velocity[] = "5";
 char Angle[] = "258";
+
+
+
+void setup() {
+  
+  
+}
+
+void loop() {
+  
+  
+  
+  // dummy data
+void LCD_Default_Data(void) {
+  LCD_Home();
+  LCD_Write("D="); LCD_Write(Total_Dis); LCD_Write("m"); LCD_Set_Cursor(0,7); LCD_Write(",T="); LCD_Write(Total_Time); LCD_Write("s");
+  LCD_Set_Cursor(1,0); LCD_Write("V="); LCD_Write(Current_velocity); LCD_Write("m/s"); LCD_Set_Cursor(1,7); LCD_Write(",A="); LCD_Write(Angle); LCD_Write("deg");
+}
