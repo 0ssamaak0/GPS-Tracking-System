7c62c452021d5523ddbbfccb867912ee695a5304
FadiiBassem <85284328+FadiiBassem@users.noreply.github.com>
2021-06-05 17:30:07 +0200

    Update LCD_Functions.c

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -29,44 +29,31 @@ void Delay(int counts, char mode[]);
 
 
 void LCD_init(void) {
-  // Rs   -> A5
-  // Rw   -> A6
-  // E    -> A7
+  LCD_Cmd_init();
+  LCD_Data_init();
+  
+  // Necessary intializing instruction
+  LCD_8Bit();
+  LCD_Move_Right();
+  LCD_Cursor_Off();
+  LCD_Clear();
+}
+
+ // Data Port
+void LCD_Data_init(void){
+  // Clock for port B
+  SYSCTL_RCGCGPIO_R |= 0X02;
+  while (!(SYSCTL_PRGPIO_R & 0X02)) {};
+
   // Data -> (B0 - B7)
-
-  // Clock for port A & B
-  SYSCTL_RCGCGPIO_R |= 0X03;
-  while (!(SYSCTL_PRGPIO_R & 0X03)) {};
-
-  GPIO_PORTA_DIR_R |= 0XE0;
-  GPIO_PORTA_DEN_R |= 0XE0;
-  GPIO_PORTA_AMSEL_R = 0X00;
-  GPIO_PORTA_AFSEL_R = 0X00;
-  GPIO_PORTA_PCTL_R = 0X00;
-  GPIO_PORTA_CR_R = 0X00;
-
+  
+  // LCD Data Port intialization
   GPIO_PORTB_DIR_R |= 0XFF;
   GPIO_PORTB_DEN_R |= 0XFF;
   GPIO_PORTB_AMSEL_R = 0X00;
   GPIO_PORTB_AFSEL_R = 0X00;
   GPIO_PORTB_PCTL_R = 0X00;
   GPIO_PORTB_CR_R = 0X00;
-
-  void LCD_Cmd(char command) {
-  GPIO_PORTA_DATA_R &= 0X1F; // R/W=0; E=0; RS=0
-  GPIO_PORTB_DATA_R = command;
-
-  GPIO_PORTA_DATA_R |= 0X80;
-  Delay(3, "ms");
-  GPIO_PORTA_DATA_R &= 0X1F;
-  
-}
-
-  LCD_8Bit();
-  LCD_Move_Right();
-  LCD_Cursor_Off();
-  LCD_Clear();
-
 }
 
 
