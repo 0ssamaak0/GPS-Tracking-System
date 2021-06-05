3bfc7a51344b1db70477b3d802b4685b810c67ce
FadiiBassem <85284328+FadiiBassem@users.noreply.github.com>
2021-06-05 12:00:21 +0200

    Update LCD_Functions.c

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -24,16 +24,30 @@
 #define NVIC_ST_RELOAD_R        (*((volatile uint32_t *)0xE000E014))
 #define NVIC_ST_CURRENT_R       (*((volatile uint32_t *)0xE000E018))
 
-void LCD_init(void) {
 
+void LCD_init(void) {
+  // Rs   -> A5
+  // Rw   -> A6
+  // E    -> A7
+  // Data -> (B0 - B7)
+
+  // Clock for port A & B
   SYSCTL_RCGCGPIO_R |= 0X03;
   while (!(SYSCTL_PRGPIO_R & 0X03)) {};
 
   GPIO_PORTA_DIR_R |= 0XE0;
   GPIO_PORTA_DEN_R |= 0XE0;
+  GPIO_PORTA_AMSEL_R = 0X00;
+  GPIO_PORTA_AFSEL_R = 0X00;
+  GPIO_PORTA_PCTL_R = 0X00;
+  GPIO_PORTA_CR_R = 0X00;
 
   GPIO_PORTB_DIR_R |= 0XFF;
   GPIO_PORTB_DEN_R |= 0XFF;
+  GPIO_PORTB_AMSEL_R = 0X00;
+  GPIO_PORTB_AFSEL_R = 0X00;
+  GPIO_PORTB_PCTL_R = 0X00;
+  GPIO_PORTB_CR_R = 0X00;
 
   LCD_8Bit();
   LCD_Move_Right();
@@ -41,7 +55,6 @@ void LCD_init(void) {
   LCD_Clear();
 
 }
-
 // Write string on screen
 void LCD_Write(char Word[]) {
   int i;
